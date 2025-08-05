0cd010e737b9a14e87549ccc6c804e17feb2c492
hamzaelsherif03 <hamza.a.elsherif@gmail.com>
2025-08-06 00:15:07 +0300

    l

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { PrivacySettings } from '@/components/privacy/privacy-compliance'
+import { PrivacySettingsDashboard } from '@/components/privacy/privacy-compliance'
 
 export default function PrivacyPage() {
   return (
     <section className="nursery-section">
       <div className="nursery-container">
-        <PrivacySettings />
+        <PrivacySettingsDashboard />
       </div>
     </section>
   )
